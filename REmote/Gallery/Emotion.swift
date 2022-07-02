90e4a077921c438a805266a5d7ad42601c1e35e9
Bohdan Arkhypchuk <arkhypchuk.b@gmail.com>
2022-07-02 18:34:49 +0300

    Emotion file

---
+++ REmote/Gallery/Emotion.swift
@@ -0,0 +1,21 @@
+//
+//  Emotion.swift
+//  REmote
+//
+//  Created by Dmytro Hetman on 29.06.2022.
+//
+
+import Foundation
+
+enum Emotion: String, CaseIterable {
+    
+    case angry
+    case disgust
+    case fear
+    case happy
+    case neutural
+    case sad
+    case surprise
+    
+}
+
